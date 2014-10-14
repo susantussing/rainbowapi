ae7efed05cd3903f7ecdd3232691ef7a09fe3bc9
Susan Tussing <susantussing@users.noreply.github.com>
2014-10-14 04:27:58 -0400

    Simplify transition for now.

--- js/app.js
+++ js/app.js
@@ -1,30 +1,21 @@
 var colorChanger = {};
 
 colorChanger.hues = ['red', 'orange', 'yellow', 'green', 'blue', 'violet'];
-colorChanger.current = 0;
-colorChanger.isMain = true;
+colorChanger.currentHue = 0;
 
-colorChanger.bottomLayer = function(){
-  if (this.isMain) {
-    return ".alternate";
-  } else {
-    return ".main";
+colorChanger.getNextHue = function(){
+  // Figure out what color we're on.
+  this.currentHue++;
+  if (this.currentHue >= this.hues.length) {
+    this.currentHue = 0;
   }
+  return this.currentHue;
 };
 
-colorChanger.getNext = function(){
-  this.current++;
-  if (this.current >= this.hues.length) {
-    this.current = 0;
-  }
-  return this.current;
-};
-
-colorChanger.setBackground = function(result) {
-  $('.main').toggleClass('show');
-  $('.alternate').toggleClass('show');
-  // $('.pause').css('color', this.hues[this.current]);
-  $(colorChanger.bottomLayer()).css('background-image','url('+result.imageUrl+')');
+colorChanger.doNext = function() {
+  // Change to the next background.
+  var result = this.thisResult;
+  $('.main').css('background-image','url('+result.imageUrl+'),url('+this.nextResult.imageUrl+')');
   $('.title').text(result.title);
   $('.title').attr('href', result.url);
   $('.creator').text(result.userName + "'s ");
@@ -36,10 +27,11 @@ colorChanger.setBackground = function(result) {
     return $('<li>').css('background-color',color).html(light+dark);
   });
   $('.colors').html(colors);
-  this.isMain ^= true;
+  // this.isMain ^= true;
 };
 
 colorChanger.getBackground = function(color) {
+  // Get a pattern from the ColourLovers.com API.
   var random = Math.floor((Math.random() * 25) + 1);
   $.ajax('http://www.colourlovers.com/api/patterns?jsonCallback=callback',{
     data: {
@@ -54,7 +46,10 @@ colorChanger.getBackground = function(color) {
     jsonp: false,
     jsonpCallback: 'callback',
     success: function(result){
-      colorChanger.setBackground(result[0]);
+      // On the first call, go ahead and set it; the first will be up twice as long but that's fine.
+      colorChanger.thisResult = colorChanger.nextResult || result[0];
+      colorChanger.nextResult = result[0];
+      colorChanger.doNext();
     },
     error: function(XHR, textStatus, errorThrown) {
         console.log(  "error: " + textStatus);
@@ -66,10 +61,13 @@ colorChanger.getBackground = function(color) {
 
 
 $(document).ready(function() {
+  // Initial call to the API.
   colorChanger.getBackground(colorChanger.hues[0]);
+
+  // Set up the automatic background shift.
   var cycle = window.setInterval(function(){
-    colorChanger.getBackground(colorChanger.hues[colorChanger.getNext()]);
-  },8000);
+    colorChanger.getBackground(colorChanger.hues[colorChanger.getNextHue()]);
+  },4000);
   var cycling = true;
   $('.pause').addClass("fa-play");
   $(document).on('click', '.circle', function(){
@@ -79,7 +77,7 @@ $(document).ready(function() {
       cycling = false;
     } else {
       cycle = window.setInterval(function(){
-        colorChanger.getBackground(colorChanger.hues[colorChanger.getNext()]);
+        colorChanger.getBackground(colorChanger.hues[colorChanger.getNextHue()]);
       },10000);
       $('.pause').removeClass("fa-pause").addClass("fa-play");
       cycling = true;
